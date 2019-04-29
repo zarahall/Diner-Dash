7e009b7dba64d26fad328ef250d8ab72ce45405c
Zara <zarahall3@gmail.com>
2019-04-29 14:08:26 -0400

    happiness

--- src/Customer.java
+++ src/Customer.java
@@ -51,6 +51,10 @@ public class Customer {
             happiness = 0;
         }else if(secondsElapsed > 60){
             happiness = 30;
+            pictureFileName = "red.png";
+        }else if(secondsElapsed > 30){
+            happiness = 70;
+            pictureFileName = "yellow.png";
         }
     }
 
