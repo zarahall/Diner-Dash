75b827a3280c33af6fbd947384f112c803c96701
saigegootman <saigegootman@gmail.com>
2019-04-29 09:34:22 -0400

    table changes

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * Class for the tables in the restaurant. The game initially starts with two tables, but as the customer increases in level,
  * more tables can be added
@@ -12,16 +14,33 @@ public class Table {
     private int x,y;
     //has the player selected this table
     private boolean isSelected = false;
+    // Table number
+    String num;
 
-    public Table(int x, int y, int w, int h){
+    public Table(int x, int y, int w, int h, Game game, String num){
         this.x = x;
         this.y = y;
         width = w;
         height = h;
+        this.game = game;
+        this.num = num;
     }
 
-    public void draw(){
-        game.getWindow().filledRectangle(x,y,width/2.0,height/2.0);
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void draw() {
+        game.getWindow().setPenColor(Color.gray);
+        game.getWindow().filledRectangle(x, y, width/2.0, height/2.0);
+    }
+
+    public void tableText(Table table) {
+        game.getWindow().text(table.getX(), table.getY(), num);
     }
 
 
